7435af5dc43c154a651847c1100eb205eaa214d3
Carlos Duarte Do Nascimento <cd@pobox.com>
2020-12-17 17:01:44 -0500

    let's compare part 1 and part 2

--- 2020/day17a.rb
+++ 2020/day17a.rb
@@ -3,37 +3,50 @@ input = File.readlines("input", chomp:true)
 ACTIVE = "#"
 INACTIVE = "."
 
-# cubes are [x,y,z] arrays. We only store the active ones
-cubes = []
+def key(x, y, z, w)
+  x + y * 100 + z * 10_000 + w * 1_000_000
+end
+
+# cubes were [x,y,z,w] arrays. We only store the active ones
+# then we switched to hashes for quicker lookup
+cubes = {}
 input.each_with_index do |line, y|
   line.split("").each_with_index do |cube, x|
     next if cube != ACTIVE
-    cubes << [x, y, 0]
+    cubes[key(x, y, 0, 0)] = [x, y, 0, 0]
   end
+end
+
+def unkey(h)
+  h.split(",").map(&:to_i)
 end
 
 def print(cubes)
-  xs, ys, zs = cubes.transpose
-  zs.uniq.sort.each do |z|
-    puts "z=#{z}"
-    ys.min.upto(ys.max) do |y|
-      line = ""
-      xs.min.upto(xs.max) do |x|
-        line << (cubes.any?([x,y,z]) ? ACTIVE : INACTIVE)
+  xs, ys, zs, ws = cubes.values.transpose
+  ws.uniq.sort.each do |w|
+    zs.uniq.sort.each do |z|
+      puts "z=#{z}, w=#{w}"
+      ys.min.upto(ys.max) do |y|
+        line = ""
+        xs.min.upto(xs.max) do |x|
+          line << (cubes.keys.any?(key(x, y, z, w)) ? ACTIVE : INACTIVE)
+        end
+        puts line
       end
-      puts line
+      puts
     end
-  puts
   end
 end
 
-def active_count_in_neighbours(cubes, xx, yy, zz)
+def active_count_in_neighbours(cubes, xx, yy, zz, ww)
   count = 0
-  (zz-1).upto(zz+1) do |z|
-    (yy-1).upto(yy+1) do |y|
-      (xx-1).upto(xx+1) do |x|
-        next if [z, y, x] == [zz, yy, xx]
-        count += 1 if cubes.any?([x, y, z])
+  (ww-1).upto(ww+1) do |w|
+    (zz-1).upto(zz+1) do |z|
+      (yy-1).upto(yy+1) do |y|
+        (xx-1).upto(xx+1) do |x|
+          next if [z, y, x, w] == [zz, yy, xx, ww]
+          count += 1 if cubes.keys.any?(key(x, y, z, w))
+        end
       end
     end
   end
@@ -41,15 +54,17 @@ def active_count_in_neighbours(cubes, xx, yy, zz)
 end
 
 def cycle(cubes)
-  new_cubes = []
-  xs, ys, zs = cubes.transpose
-  (zs.min - 1).upto(zs.max + 1) do |z|
-    (ys.min - 1).upto(ys.max + 1) do |y|
-      (xs.min - 1).upto(xs.max + 1) do |x|
-        active = cubes.any?([x, y, z])
-        if (active && active_count_in_neighbours(cubes, x, y, z).between?(2, 3)) ||
-           (!active && active_count_in_neighbours(cubes, x, y, z) == 3)
-          new_cubes << [x, y, z]
+  new_cubes = {}
+  xs, ys, zs, ws = cubes.values.transpose
+  (ws.min - 1).upto(ws.max + 1) do |w|
+    (zs.min - 1).upto(zs.max + 1) do |z|
+      (ys.min - 1).upto(ys.max + 1) do |y|
+        (xs.min - 1).upto(xs.max + 1) do |x|
+          active = cubes.keys.any?(key(x, y, z, w))
+          if (active && active_count_in_neighbours(cubes, x, y, z, w).between?(2, 3)) ||
+            (!active && active_count_in_neighbours(cubes, x, y, z, w) == 3)
+            new_cubes[key(x, y, z, w)] = [x, y, z, w]
+          end
         end
       end
     end
