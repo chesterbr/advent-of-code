d47b72466ecd05e181a586fb5d4a37108f0b99f3
Carlos Duarte Do Nascimento <cd@pobox.com>
2020-12-25 10:53:49 -0500

    Simplify/consolidate the loop size search and key search
    This actually worsens the execution time a bit, but still O(N) (and is MUCH more readable)

--- 2020/day25.rb
+++ 2020/day25.rb
@@ -1,41 +1,20 @@
-#  input = File.readlines("input", chomp:true)
-
-def transform(subject, loop_size)
+# Receives either a known loop size or a known value,
+# returns the other
+def transform(subject, known_loop_size: nil, known_value: nil)
   value = 1
-  loop_size.times do
-    value = (value * subject) % 20201227
-  end
-  value
-end
-
-
-@memo = []
-def transform_recursive(subject, loop_size)
-  return 1 if loop_size == 0
-  @memo[subject] ||= []
-  @memo[subject][loop_size] ||= (transform_recursive(subject, loop_size - 1) * subject) % 20201227
-end
-
-def loop_size_from_pk(pk)
   1.upto(10000000) do |loop_size|
-    return loop_size if pk == transform_recursive(7, loop_size)
+    value = (value * subject) % 20201227
+    return loop_size if value == known_value
+    return value if loop_size == known_loop_size
   end
-  raise "loop size not found for #{pk}"
 end
-
-# card_pk = transform(7, card_loop_size)
-# door_pk = transform(7, card_loop_size)
 
 # known_card_pk = 5764801
 # known_door_pk = 17807724
-known_card_pk = 8335663   # 5764801
-known_door_pk = 8614349   # 17807724
+known_card_pk = 8335663
+known_door_pk = 8614349
 
-card_loop_size = loop_size_from_pk(known_card_pk)
-puts card_loop_size
-puts transform(7, card_loop_size)
-# door_loop_size = loop_size_from_pk(known_door_pk)
-
-puts transform(known_door_pk, card_loop_size)
+card_loop_size = transform(7, known_value: known_card_pk)
+puts transform(known_door_pk, known_loop_size: card_loop_size)
 
 
